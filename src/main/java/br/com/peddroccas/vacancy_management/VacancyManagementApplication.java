0ce49f54994097f81959e8e0bc78bd9797e9322a
Pedro Antunes <pedroabernardes11@gmail.com>
2024-10-31 14:21:14 -0300

    feat:initial commit

---
+++ src/main/java/br/com/peddroccas/vacancy_management/VacancyManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.peddroccas.vacancy_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyManagementApplication.class, args);
+	}
+
+}
