e8609f5ccbc21297d74201978422d9aedb68cbc4
Pedro Antunes <pedroabernardes11@gmail.com>
2024-11-07 13:20:41 -0300

    feat: securty filter

---
+++ src/main/java/br/com/peddroccas/vacancy_management/providers/JWTProvider.java
@@ -0,0 +1,34 @@
+package br.com.peddroccas.vacancy_management.providers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@Service
+public class JWTProvider {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    public String validateToken(String token) {
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        token = token.replace("Bearer ", "");
+
+        try {
+            var subject = JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+            return subject;
+        } catch (JWTVerificationException e) {
+            return "";
+        }
+
+    }
+}
