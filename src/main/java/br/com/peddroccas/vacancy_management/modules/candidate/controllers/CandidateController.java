fbc415b75b41c5e1ce2468c1b91cb5768897573e
Pedro Antunes <pedroabernardes11@gmail.com>
2024-11-07 14:33:30 -0300

    feat: get profile

--- src/main/java/br/com/peddroccas/vacancy_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/peddroccas/vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,7 @@
 package br.com.peddroccas.vacancy_management.modules.candidate.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.peddroccas.vacancy_management.modules.candidate.CandidateEntity;
 import br.com.peddroccas.vacancy_management.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.peddroccas.vacancy_management.modules.candidate.useCases.ProfileCandidateUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    ProfileCandidateUseCase profileCandidateUseCase;
     @Autowired
     private CreateCandidateUseCase createCandidateUseCase;
 
@@ -23,6 +30,18 @@ public class CandidateController {
         try {
             var result = this.createCandidateUseCase.execute(candidate);
             return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> get(HttpServletRequest request) {
+
+        var idCandidate = request.getAttribute("candidate_id");
+        try {
+            var profile = this.profileCandidateUseCase.execute(UUID.fromString(idCandidate.toString()));
+            return ResponseEntity.ok().body(profile);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
